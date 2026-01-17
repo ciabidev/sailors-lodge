129abcee84fc2f8dd812ed4a0be0711701756b66
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2026-01-16 22:53:10 -0500

    chore: change text for ping command

--- commands/system/ping.js
+++ commands/system/ping.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('ping').setDescription('check if Karoo is ok'),
+	data: new SlashCommandBuilder().setName('ping').setDescription('check if discord servers are ok'),
 	async execute(interaction) {
 		await interaction.reply('hi');
 	},

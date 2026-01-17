e752787c805f132e6d802f58338e95317e36a14e
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2026-01-16 23:00:54 -0500

    refactor: handle party editing interaction in interactionCreate.js

--- src/modules/editParty.js
+++ src/modules/editParty.js
@@ -9,75 +9,33 @@ const {
 } = require("discord.js");
 
 module.exports = async function editParty(interaction, party) {
-    if (!party) {
-      return interaction.reply({
-        content: "This party does not exist.",
-        flags: MessageFlags.Ephemeral,
-      });
-    }
+  if (!party) {
+    return interaction.reply({
+      content: "This party does not exist.",
+      flags: MessageFlags.Ephemeral,
+    });
+  }
 
-let currentParty = await interaction.client.modules.db.getCurrentParty(interaction.user.id);
-if (!currentParty) {
-return interaction.reply({
-    content: "You are not in a party.",
-    flags: [MessageFlags.Ephemeral],
-});
-}
+  let currentParty = await interaction.client.modules.db.getCurrentParty(interaction.user.id);
+  if (!currentParty) {
+    return interaction.reply({
+      content: "You are not in a party.",
+      flags: [MessageFlags.Ephemeral],
+    });
+  }
   if (interaction.user.id !== party.host.id) {
     return interaction.reply({
       content: "Only the party leader can edit.",
       flags: [MessageFlags.Ephemeral],
     });
   }
-  // Open modal for editing
+
   await interaction.client.modules.partyConfigModal(interaction, {
     name: party.name,
     description: party.description || "",
     limit: party.memberLimit || 10,
     visibility: party.visibility || "public",
-  });
-  
-  let editModal;
-  try {
-    editModal = await interaction.awaitModalSubmit({
-      filter: (i) => i.customId === "party-modal" && i.user.id === party.host.id,
-      time: 60_000,
-    });
-  } catch (error) {
-    // Collector timed out or was rejected
-    if (error.code === 'InteractionCollectorError') {
-      return interaction.followUp({
-        content: "Modal submission timed out. Please try again.",
-        flags: [MessageFlags.Ephemeral],
-      });
-    }
-    throw error;
-  }
+  }, customId = `party-modal:${party._id}`);
 
-  party = await interaction.client.modules.db.updateParty(
-    party._id,
-    {
-      $set: {
-        name: editModal.fields.getTextInputValue("name"),
-        description: editModal.fields.getTextInputValue("description"),
-        memberLimit: parseInt(editModal.fields.getTextInputValue("limit")) || 10,
-        visibility: editModal.fields.getStringSelectValues("visibility")[0],
-      },
-    },
-    interaction
-  );
-  try {
-  await editModal.update({
-    components: await interaction.client.modules.renderPartyCard(party, interaction),
-  }); // editModal is so impatient for a response lol so we have to put it before the long updateParty
-  } catch (err) {
-    if (err.code === 10008) { // this module was likely called by a slash command
-      editModal.reply({
-        content: "edited party",
-        flags: [MessageFlags.Ephemeral],
-      });
-    }
-  }
-  await interaction.client.modules.updatePartyCards(interaction, party);
-  return;
+  return
 };

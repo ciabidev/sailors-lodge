963054718b41823e607f9a4292214f5b08dd3939
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2026-01-22 16:32:44 -0500

    fix js error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 // Render automatically assigns a port in process.env.PORT
 app.listen(process.env.PORT || 8000, () => {
   console.log(`🌐 Express keep-alive server running on port ${process.env.PORT || 8000}`);
-}
+}),
 
 // INDEX.JS COPY PASTE TEMPLATE
 
